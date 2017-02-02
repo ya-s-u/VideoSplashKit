0af36317c7cedeb7c6d2a8f4c28e9d2a08b2427a
Yasuaki Goto <yasu1003@gmail.com>
2017-02-02 18:26:31 +0900

    [fix] access control

--- VideoSplashKit/Source/VideoSplashViewController.swift
+++ VideoSplashKit/Source/VideoSplashViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import MediaPlayer
 import AVKit
 
-public enum ScalingMode {
+open enum ScalingMode {
     case resize
     case resizeAspect
     case resizeAspectFill
@@ -69,7 +69,7 @@ open class VideoSplashViewController: UIViewController {
         }
     }
     
-    override public func viewDidAppear(_ animated: Bool) {
+    override open func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         moviePlayer.view.frame = videoFrame
         moviePlayer.view.backgroundColor = self.backgroundColor;
@@ -90,7 +90,7 @@ open class VideoSplashViewController: UIViewController {
         }
     }
     
-    public override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+    open override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         
         guard let player = object as? AVPlayer else {
             super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
